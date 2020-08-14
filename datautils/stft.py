c01a78d7e3bc6de1743c21e590fb04d83360f239
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-08-14 15:16:07 -0700

    stft class now works with helper class data loader

--- datautils/stft.py
+++ datautils/stft.py
@@ -14,106 +14,100 @@ from scipy.fftpack import fft, ifft
 import pywt
 from PyEMD import EMD
 
-EXCLUDE_CHANNELS = ['Status', 'EXG1', 'EXG2', 'EXG3', 'EXG4', 'EXG5', 'EXG6', 'EXG7', 'EXG8']
+EXCLUDED_CHANNELS = ['Status', 'EXG1', 'EXG2', 'EXG3', 'EXG4', 'EXG5', 'EXG6', 'EXG7', 'EXG8']
 
 class STFT(object):
     """
     Class used to help create spectrogram images via STFT for EEG data
     """
-    def __init(self, **kwargs):
+    def __init__(self, **kwargs):
         """
         Default constructor
         """
         self.classes = kwargs['state']
         self.root_path = kwargs['root_path']
-        self.all_files = kwargs['all_files']
+        self.data_helper = kwargs['data_helper']
+
+    def generate_stft_transform(self):
+        """
+        Function used to generate the short-time fourier transform
+        """
+        self.data_helper.clean_create_dir(self.root_path)
+
+        for class_label in self.data_helper.all_files.keys():
+            class_files = self.data_helper.all_files[class_label]
+
+            class_root = os.path.join(self.root_path, str(class_label))
+            self.data_helper.clean_create_dir(class_root)
+
+            for eeg_file in class_files:
+                # Load EEG data
+                raw = self.data_helper.load_data(eeg_file)
+
+                # Apply filter to data
+                raw.filter(0.5, 32.0, fir_design='firwin')
+
+                # Create output dir to patient data
+                filename_dir = eeg_file.split(os.path.sep)[-4:-3]
+                patient_path = os.path.join(class_root, '/'.join(filename_dir))
+                self.data_helper.clean_create_dir(patient_path)
+
+                # iterate EEG data for patient
+                self.iterate_eeg_data(data=raw, output_dir=patient_path)
 
     def iterate_eeg_data(self, **kwargs):
         """
         Function used to iterate data, and generate the spectrogram images
         from the short time fourier transform of an EEG signal
         """
-        pass
+        raw_data = kwargs["data"].copy()
+        kwargs["data"].load_data()
+        data = kwargs["data"].to_data_frame()
 
-    def generate_stft_transform(self):
-        """
-        Function used to generate the short-time fourier transform
-        """
+        # Get list of channel names
+        channel_names = raw_data.ch_names
 
-        if (self.classes == 'ALL'):
-            class_list = ['NONPD', 'PD']
-        else:
-            class_list = [self.classes]
+        # Sample Frequency
+        fs = int(raw_data.info["sfreq"])
 
-        if os.path.isdir(self.root_path):
-            shutil.rmtree(self.root_path, ignore_errors=True)
+        # STFT Parameters
+        segment_size = 1024 # 4 seconds
+        amp = 5
 
-        os.makedirs(self.root_path)
+        for channel in channel_names:
+            if channel in EXCLUDED_CHANNELS:
+                continue
+            # Create channel output directory and iterate through all channels
+            channel_path = os.path.join(kwargs["output_dir"], channel)
+            self.data_helper.clean_create_dir(channel_path)
 
-        for curr_class in class_list:
-            if curr_class == 'PD':
-                self.handle_PD_patients()
+            # counter for image names e.g. ~/masters-project/spectrogram-images/NONPD/sub-hc4/ses-hc/Fp1/0'
+            image_counter = 0
 
-            if curr_class == 'NONPD':
-                self.handle_NONPD_patients()
+            channel_data = data[channel].values
+            size = len(channel_data)
+            segments = int(size // segment_size)
 
-    def handle_PD_patients(self, **kwargs):
-        """
-        Funtion used to handle PD patients specifically.
-        """
-        # Need to iterate through all patient data
-        class_root = os.path.join(kwargs['root_path'], 'PD')
-        clean_and_create(class_root)
+            for index in range(segments):
+                lower_point = index * segment_size
+                upper_point = lower_point + segment_size
+                current_segment = channel_data[lower_point : upper_point]
 
-        for subject_name in kwargs['all_files']['PD']:
-            subject_files = kwargs['all_files']['PD'][subject_name]
+                f, t, Zxx = signal.stft(current_segment, fs, window='blackman', nperseg=256, boundary=None, noverlap=230)
 
-            for filename in subject_files:
-                """
-                    1. Need to load in the data
-                """
-                data = load_data(filename)
+                Zxx = Zxx[0 : 17]
+                f = f[0 : 17]
 
-                # apply filters
-                data.filter(0.5, 32.0, fir_design='firwin')
+                try:
+                    output_filepath = os.path.join(channel_path, str(image_counter))
+                    plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=amp, shading='gouraud')
+                    plt.axis('off')
 
-                """
-                    2. Create output dir for patient data
-                """
-                patient_path = get_patient_path(filename, class_root, 'PD')
-                clean_and_create(patient_path)
+                    figure = plt.gcf()
+                    figure.set_size_inches(1.69, 1.69)
+                    plt.savefig(output_filepath, bbox_inches='tight', pad_inches=0, dpi=100)
+                    plt.clf()
 
-                """
-                    3. Create Spectrogram images from the data
-                """
-                stft_iterate_eeg_data(data=data, output_dir=patient_path)
-
-    def handle_NONPD_patients(self, **kwargs):
-        """
-        Function used to handle NONPD patients specifically.
-        """
-        # Make directory for class (e.g., NONPD)
-        class_root = os.path.join(kwargs["root_path"], 'NONPD')
-        clean_and_create(class_root)
-
-        # need to read every patient EEG reading
-        for filename in kwargs["all_files"]['NONPD']:
-            """
-                1. Need to load in the data
-            """
-            data = load_data(filename)
-
-            # apply filters
-            data.filter(0.5, 32.0, fir_design='firwin')
-
-            """
-                2. Create output dir for patient data
-            """
-            patient_path = get_patient_path(filename, class_root, 'NONPD')
-            clean_and_create(patient_path)
-
-            """
-                3. Create spectrogram images from the data
-            """
-            stft_iterate_eeg_data(data=data, output_dir=patient_path)
-
+                    image_counter += 1
+                except FloatingPointError as e:
+                    print('Caught divide by 0 error: {0}'.format(output_filepath))
