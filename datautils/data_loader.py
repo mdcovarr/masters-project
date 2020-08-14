9147feb41c596e064c29649f23a2a84e4d7005e5
Michael Covarrubias <mdcovarrubias@gmail.com>
2020-08-14 13:59:16 -0700

    creating a data loader helper class.
    data loader class will be used to get path to all EEG files
    and separate them by class

---
+++ datautils/data_loader.py
@@ -0,0 +1,74 @@
+"""
+    Script to help load EEG data files, separating files by class type
+"""
+
+import mne
+import glob
+import pandas
+import shutil
+import numpy as np
+import math
+import os
+import re
+from scipy import signal
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+
+class DataLoader(object):
+    """
+        Data Loader class to help import data and separate by class
+    """
+    def __init__(self, **kwargs):
+        """
+        Default Constructor
+        """
+        self.paths = kwargs['paths']
+
+    def load_data_files(self, locations):
+        """
+        Function used to get all files of interest in regards to
+        EEG data. Locations is a list of different paths where each entry
+        is a path for a certain class
+        :param locations: list of location paths for each class
+        """
+        all_files = {}
+
+        for index, location in enumerate(locations):
+            class_files = glob.glob(location, recursive=True)
+            all_files[str(index)] = class_files
+
+        self.all_files = all_files
+
+    def clean_create_dir(self, path):
+        """
+        Function used to clear out a directory nd create a new one
+        :param path: directory path to clean and create
+        """
+        if os.path.isdir(path):
+            shutil.rmtree(path, ignore_errors=True)
+
+        os.makedirs(path)
+
+    def load_data(self, filename):
+        """
+        Function used to load EEG data
+        :param filename: the file name of .bdf file we want to read in
+        :return raw: the raw data of given filename
+        """
+        raw = mne.io.read_raw_bdf(filename, preload=True, stim_channel='auto', verbose=False)
+
+        """
+            Informative parameters
+
+            raw_data.info["sfreq"]
+            raw_data.info["nchan"]
+            raw_data.ch_names
+        """
+        return raw
+
+if __name__ == '__main__':
+    paths = ['../data/**/ses-hc/eeg/*.bdf', '../data/**/ses-off/eeg/*.bdf', '../data/**/ses-on/eeg/*.bdf']
+    dl = DataLoader(paths=paths)
+    dl.load_data_files(paths)
+
+    print(dl.all_files)
